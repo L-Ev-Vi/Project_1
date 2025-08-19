bcc5769e5e0201ad02db50657e44f9f767f5af4d
Evgenii_Lobachev <lobachev_e_96@mail.ru>
2025-08-19 18:52:23 +0300

    Исправлены баги в модуле widget.py

--- src/widget.py
+++ src/widget.py
@@ -1,4 +1,4 @@
-#__package__ = "src"
+# __package__ = "src"
 
 from datetime import datetime
 
@@ -8,14 +8,17 @@ from .masks import get_mask_account, get_mask_card_number
 def mask_account_card(data: str) -> str:
     """Принимать один строку, содержащую тип и номер карты или счета.
     Возвращать строку с замаскированным номером."""
+    data_name = ""
     data_number = ""
     for letter in data:
-        if letter.isalnum():
+        if letter.isdigit():
             data_number += letter
+        else:
+            data_name += letter
     if len(data_number) > 16:
-        return get_mask_account(int(data_number))
+        return data_name + get_mask_account(int(data_number))
     else:
-        return get_mask_card_number(int(data_number))
+        return data_name + get_mask_card_number(int(data_number))
 
 
 def get_date(date_: str) -> str:
