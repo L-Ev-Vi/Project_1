14bdafd400604ae0c9d66a8b4375f6eaa90ef866
Evgenii_Lobachev <lobachev_e_96@mail.ru>
2025-08-24 14:01:34 +0300

    update processing.py

--- src/processing.py
+++ src/processing.py
@@ -1,7 +1,7 @@
-from typing import Iterable
+from typing import Any, Iterable
 
 
-def filter_by_state(original_list: Iterable[list[dict]], state: str = "EXECUTED") -> list[dict]:
+def filter_by_state(original_list: Iterable[list[dict[str, Any]]], state: str = "EXECUTED") -> list:
     """Функция принимает список словарей и 'ЗНАЧЕНИЕ' для ключа (НЕ КЛЮЧ), (если не задано то по умолчанию 'EXECUTED'),
     и возвращает новый список словарей, содержащий только те словари,
     у которых ключ соответствует указанному значению."""
@@ -13,7 +13,7 @@ def filter_by_state(original_list: Iterable[list[dict]], state: str = "EXECUTED"
     return new_list
 
 
-def sort_by_date(original_list: Iterable[list[dict]], sort_order: bool = True) -> list:
+def sort_by_date(original_list: Iterable[list[dict[str, Any]]], sort_order: bool = True) -> list:
     """Функция принимает список словарей и параметр 'bool' задающий порядок сортировки (по умолчанию — убывание),
     и возвращает новый список, отсортированный по дате ('date')"""
-    return sorted(original_list, key=lambda dic: dic["date"], reverse = sort_order)
+    return sorted(original_list, key=lambda dic: dic["date"], reverse=sort_order)
