ffa049a1a2a01e6642953f4a125be51b46f8b81c
Evgenii_Lobachev <lobachev_e_96@mail.ru>
2025-09-03 21:09:08 +0300

    update homework_10_2

--- src/widget.py
+++ src/widget.py
@@ -1,4 +1,4 @@
-from datetime import datetime
+from dateutil import parser
 
 from .masks import get_mask_account, get_mask_card_number
 
@@ -7,7 +7,7 @@ def mask_account_card(data: str = None) -> str:
     """Принимать одну строку, содержащую тип и номер карты или счета.
     Возвращать строку с замаскированным номером."""
     if not isinstance(data, str):
-        raise TypeError ("Ошибка ввода")
+        raise TypeError("Ошибка ввода")
     else:
         data_name = ""
         data_number = ""
@@ -17,16 +17,17 @@ def mask_account_card(data: str = None) -> str:
             else:
                 data_name += letter
         if data_name == "" or data_number == "":
-            raise AssertionError ("Не полный ввод данных")
+            raise AssertionError("Не полный ввод данных")
         if len(data_number) > 19:
             return data_name + get_mask_account(int(data_number))
         else:
             return data_name + get_mask_card_number(int(data_number))
 
 
-def get_date(date_: str) -> str:
+def get_date(date_: str = None) -> str:
     """Принимает на вход строку с датой в формате '2024-03-11T02:26:18.671407'
     и возвращает строку с датой в формате 'ДД.ММ.ГГГГ'"""
-    date_only = date_[:10]
-    date_format = datetime.strptime(date_only, "%Y-%m-%d").date()
+    if not isinstance(date_, str):
+        raise TypeError("Ошибка ввода даты")
+    date_format = parser.parse(date_)
     return date_format.strftime("%d.%m.%Y")
--- tests/test_widget.py
+++ tests/test_widget.py
@@ -2,14 +2,15 @@ import pytest
 
 from src.widget import mask_account_card, get_date
 
+
 @pytest.mark.parametrize('data, result', [('Visa Platinum 7000792289606361', 'Visa Platinum 7000 79** **** 6361'),
                                           ('Счет 73654108430135874305', 'Счет **4305'),
                                           ('Visa Platinum 8990922113665229', 'Visa Platinum 8990 92** **** 5229'),
                                           ('Visa Gold 15684', 'Visa Gold ***84'),
                                           ('MasterCina 6571552397456239821', 'MasterCina 6571 55** **** **** 821')])
-
 def test_mask_account_card_positive(data, result):
     assert mask_account_card(data) == result
+
 
 def test_mask_account_card_negative():
     with pytest.raises(TypeError) as info:
@@ -17,9 +18,11 @@ def test_mask_account_card_negative():
 
     assert str(info.value) == "Ошибка ввода"
 
+
 def test_mask_account_card_negative_name():
     with pytest.raises(AssertionError) as info:
         mask_account_card('7000792289606361')
+
 
 def test_mask_account_card_negative_number():
     with pytest.raises(AssertionError) as info:
@@ -30,3 +33,28 @@ def test_mask_account_card_negative_number():
 
 def test_get_date_positive(date_):
     assert get_date(date_) == "11.03.2024"
+
+
+@pytest.mark.parametrize('date, result', [('2020-11-14 15:43:32.249588', '14.11.2020'),
+                                          ('2020-11-14', '14.11.2020'),
+                                          ('2020-10-17 09:40:33.614581+02:00', '17.10.2020'),
+                                          ('2020.11.14 15:43:32.249588', '14.11.2020'),
+                                          ('2020/11/14 15:43:32.249588', '14.11.2020'),
+                                          ('Thursday, 17. October 2019 5:10PM', '17.10.2019'),
+                                          ('Thu Oct 17 17:10:28 2019', '17.10.2019')
+                                          ])
+def test_get_date_positive_value(date, result):
+    assert get_date(date) == result
+
+
+def test_get_date_negative():
+    with pytest.raises(TypeError) as info:
+        get_date(2020 - 11 - 14)
+
+    assert str(info.value) == "Ошибка ввода даты"
+
+
+def test_get_date_negative_zero():
+    with pytest.raises(TypeError) as info:
+        get_date()
+    assert str(info.value) == "Ошибка ввода даты"
