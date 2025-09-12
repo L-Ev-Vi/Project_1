426ddbf488ee188404ca2e709003b1cb377a1f73
Evgenii_Lobachev <lobachev_e_96@mail.ru>
2025-09-12 23:53:37 +0300

    update generators.py

--- src/generators.py
+++ src/generators.py
@@ -1,7 +1,22 @@
-from typing import Iterator
+from typing import Generator, Iterator
 
 
-def filter_by_currency(transactions: list) -> Iterator:
+def filter_by_currency(transactions: list, currency: str) -> Iterator:
     """ Функция принимает на вход список словарей представляющих транзакции и вид валюты, результатом функции,
      является итератор, который поочередно выдает транзакции."""
-    pass
+    operations_list = []
+    if not transactions:
+        while True:
+            #Можно выводить уведомление "Нет операций с волютой {currency}"
+            yield {}
+    else:
+        for operation in transactions:
+            if any(currency_data == currency for values in operation.values() if type(values) is dict for value
+               in values.values() if type(value) is dict for currency_data in value.values()):
+                operations_list.append(operation)
+        for i, operation_data in enumerate(operations_list):
+            yield operation_data
+            if i == len(operations_list) - 1:
+                while True:
+                    # Можно выводить уведомление "Больше нет операций с волютой {currency}"
+                    yield {}
---
+++ tests/test_generators.py
@@ -0,0 +1,67 @@
+import pytest
+
+from src.generators import filter_by_currency
+
+
+def test_filter_by_currency_USD(list_transactions):
+    gen = filter_by_currency(list_transactions,"USD")
+
+    result = {"id": 939719570,
+              "state": "EXECUTED",
+              "date": "2018-06-30T02:08:58.425572",
+              "operationAmount": {"amount": "9824.07",
+                                  "currency": {"name": "USD",
+                                               "code": "USD"}},
+              "description": "Перевод организации",
+              "from": "Счет 75106830613657916952",
+              "to": "Счет 11776614605963066702"
+              }
+
+    assert next(gen) == result
+    assert next(gen) == {"id": 142264268,
+             "state": "EXECUTED",
+             "date": "2019-04-04T23:20:05.206878",
+             "operationAmount": {"amount": "79114.93",
+                                 "currency": {"name": "USD",
+                                              "code": "USD"}},
+             "description": "Перевод со счета на счет",
+             "from": "Счет 19708645243227258542",
+             "to": "Счет 75651667383060284188"
+             }
+    assert next(gen) == {}
+    assert next(gen) == {}
+
+
+def test_filter_by_currency_RUB(list_transactions):
+    gen = filter_by_currency(list_transactions,"RUB")
+
+    assert next(gen) == {"id": 939719570,
+             "state": "EXECUTED",
+             "date": "2018-06-30T02:08:58.425572",
+             "operationAmount": {"amount": "9824.07",
+                                 "currency": {"name": "RUB",
+                                              "code": "RUB"}},
+             "description": "Перевод организации",
+             "from": "Счет 75106830613657916952",
+             "to": "Счет 11776614605963066702"
+             }
+    assert next(gen) == {"id": 142264268,
+             "state": "EXECUTED",
+             "date": "2019-04-04T23:20:05.206878",
+             "operationAmount": {"amount": "79114.93",
+                                 "currency": {"name": "RUB",
+                                              "code": "RUB"}},
+             "description": "Перевод со счета на счет",
+             "from": "Счет 19708645243227258542",
+             "to": "Счет 75651667383060284188"
+             }
+    assert next(gen) == {}
+    assert next(gen) == {}
+
+def test_filter_by_currency_zero():
+    resul = {}
+    gen = filter_by_currency([], "USD")
+    assert next(gen) == resul
+    assert next(gen) == {}
+    assert next(gen) == resul
+
