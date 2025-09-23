55e5c861f64eb6bab4c28490f4a725bee32f7a0b
Evgenii_Lobachev <lobachev_e_96@mail.ru>
2025-09-23 23:28:04 +0300

    update decorators.py update test_decorators.py

--- src/decorators.py
+++ src/decorators.py
@@ -11,7 +11,21 @@ def log(filename: Optional = None):
     def wrapper(func: Callable):
         @wraps(func)
         def inner(*args, **kwargs):
-            pass
+            call_time = f"Время вызова: {datetime.datetime.now()}\n"
+            function_name = f"Функция: {func.__name__}"
+            arguments_passed = f"Передаваемые аргументы:{[args], [kwargs]}"
+            try:
+                result = func(*args, **kwargs)
+                execution_result = f"Результат: {result}"
+            except Exception as n:
+                execution_result = f"Результат: {func.__name__} error: {str(n)} Inputs: {arguments_passed}"
+            if filename:
+                with open('../date/filename', 'a', encoding='UTF-8') as f:
+                    f.write(f"{call_time}\n"
+                            f"{function_name}\n"
+                            f"{execution_result}")
+            else:
+                print(f"{call_time}\n {function_name}\n {execution_result}")
 
         return inner
 
