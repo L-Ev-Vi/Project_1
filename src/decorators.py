60c82bd46e8b03ec7c44b459f90c28a23c334f4c
Evgenii_Lobachev <lobachev_e_96@mail.ru>
2025-09-26 01:58:23 +0300

    update decorators.py update test_decorators.py

--- src/decorators.py
+++ src/decorators.py
@@ -1,31 +1,37 @@
 import datetime
-from typing import Optional, Callable
+import time
 from functools import wraps
+from typing import Callable, Optional
 
 
 def log(filename: Optional = None):
     """Декоратор 'log', автоматически регистрирует детали выполнения передаваемой функций,
-    такие как время вызова и конец выполнения, имя функции, передаваемые аргументы,
-    результат выполнения и информация об ошибках, в заданный файл или в консоль если файл сохранения не задан"""
+        такие как время вызова и конец выполнения, имя функции, передаваемые аргументы,
+        результат выполнения и информация об ошибках, в заданный файл или в консоль если файл сохранения не задан."""
 
     def wrapper(func: Callable):
         @wraps(func)
         def inner(*args, **kwargs):
-            call_time = f"Время вызова: {datetime.datetime.now()}\n"
+            # start_time = time.time()
+            call_time = f"Дата и время вызова: {datetime.datetime.now()}"
             function_name = f"Функция: {func.__name__}"
-            arguments_passed = f"Передаваемые аргументы:{[args], [kwargs]}"
+            arguments_passed = f"{args, kwargs}"
             try:
-                result = func(*args, **kwargs)
-                execution_result = f"Результат: {result}"
-            except Exception as n:
-                execution_result = f"Результат: {func.__name__} error: {str(n)} Inputs: {arguments_passed}"
-            if filename:
-                with open('../date/filename', 'a', encoding='UTF-8') as f:
-                    f.write(f"{call_time}\n"
-                            f"{function_name}\n"
-                            f"{execution_result}")
+                result_fanc = func(*args, **kwargs)
+                execution_result = f"Результат: {result_fanc}"
+            except Exception as e:
+                execution_result = f"Результат: {func.__name__} error: {str(e)}, Inputs: {arguments_passed}"
+                return None
             else:
-                print(f"{call_time}\n {function_name}\n {execution_result}")
+                return result_fanc
+            finally:
+                # end = time.time()
+                # lead_time = f"Время выполнения {end - start_time}"
+                if filename:
+                    with open('../data/' + filename, 'a', encoding='UTF-8') as f:
+                        f.write(f"{call_time}\n{function_name}\n{execution_result}\n")
+                else:
+                    print(f"{call_time}\n{function_name}\n{execution_result}\n")
 
         return inner
 
