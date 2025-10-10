2021e3a81464db6bce471bbb753d66b9deb25476
Evgenii_Lobachev <lobachev_e_96@mail.ru>
2025-10-10 20:28:44 +0300

    update programs

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -83,8 +83,8 @@ class TestFrom(unittest.TestCase):
             {
                 'id': 939719570,
                 'state': 'EXECUTED',
-                "date": "2018-06-30T02:08:58.425572",
-                "operationAmount": {"amount": "9824.07", "currency": {"name": "USD", "code": "USD"}},
+                'date': '2018-06-30T02:08:58.425572',
+                'operationAmount': {"amount": "9824.07", "currency": {"name": "USD", "code": "USD"}},
                 "description": "Перевод организации",
                 "from": "Счет 75106830613657916952",
                 "to": "Счет 11776614605963066702",
