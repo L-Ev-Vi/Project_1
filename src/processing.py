7afa028ad8287acaad8c63025e68e206a9401003
Evgenii_Lobachev <lobachev9614@gmail.com>
2025-10-29 00:21:28 +0300

    update processing.py in programs

--- src/processing.py
+++ src/processing.py
@@ -1,10 +1,16 @@
+import json
+import re
+from collections import Counter
+
 from dateutil import parser
 
 
 def filter_by_state(original_list: list, status: str = "EXECUTED") -> list[dict]:
-    """Функция принимает список словарей и значение для ключа 'state', (если не задано то по умолчанию 'EXECUTED'),
+    """
+    Функция принимает список словарей и значение для ключа 'state', (если не задано то по умолчанию 'EXECUTED'),
     и возвращает новый список словарей, содержащий только те словари,
-    у которых ключ соответствует указанному значению."""
+    у которых ключ соответствует указанному значению.
+    """
     new_list = []
     if not isinstance(original_list, list):
         return []
@@ -16,10 +22,45 @@ def filter_by_state(original_list: list, status: str = "EXECUTED") -> list[dict]
 
 
 def sort_by_date(original_list: list, sort_order: bool = True) -> list:
-    """Функция принимает список словарей и параметр 'bool' задающий порядок сортировки (по умолчанию — убывание),
-    и возвращает новый список, отсортированный по дате ('date')"""
+    """
+    Функция принимает список словарей и параметр 'bool' задающий порядок сортировки (по умолчанию — убывание),
+    и возвращает новый список, отсортированный по дате ('date')
+    """
     for di in original_list:
         date_format = parser.parse(di["date"])
         date_string = date_format.strftime("%Y-%m-%dT%H:%M:%S.%f")
         di["date"] = date_string
     return sorted(original_list, key=lambda dic: dic["date"], reverse=sort_order)
+
+
+def process_bank_search(data: list[dict], search: str) -> list[dict]:
+    """
+    Функция, принимает список словарей с данными о банковских операциях и строку поиска, а возвращает список словарей,
+    у которых в описании (в 'description') есть данная строка.
+    """
+    new_list = [case for case in data if re.search(f'({search})', str(case.get("description")), flags=re.I)]
+
+    return new_list
+
+
+def process_bank_operations(data: list[dict], categories: list) -> dict:
+    """Функция, принимает список словарей с данными о банковских операциях и список категорий операций,
+    а возвращать словарь, в котором ключи — это названия категорий, а значения — это количество операций
+    в каждой категории. Категории операций хранятся в поле 'description'.
+    """
+    counter_operations = Counter([case["description"] for case in data if case.get("description") in categories])
+    for category in categories:
+        if category not in counter_operations:
+            counter_operations[category] = 0
+
+    return counter_operations
+
+# if __name__ == '__main__':
+#     with open('data/operations.json', 'r', encoding='utf-8') as file:
+#         operations = json.load(file)
+#
+#     # print(process_bank_search(operations, "ПЕРЕВОД"))
+#
+#     categories_list = list(set(operation["description"] for operation in operations if operation.get("description")))
+#
+#     print(process_bank_operations(operations, categories_list))
--- tests/test_processing.py
+++ tests/test_processing.py
@@ -1,6 +1,6 @@
 import pytest
 
-from src.processing import filter_by_state, sort_by_date
+from src.processing import filter_by_state, sort_by_date, process_bank_search, process_bank_operations
 
 
 def test_filter_by_state_positive(list_dic):
@@ -51,44 +51,130 @@ def test_sort_by_date_positive_positive_rivers(list_dic):
     "list_d, result",
     [
         (
-            [
-                {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
-                {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
-                {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
-                {"id": 594226727, "state": "EXECUTED", "date": "2018-09-12T21:27:25.241689"},
-                {"id": 594226727, "state": "EXECUTED", "date": "2018-09-12T21:27:25.241689"},
-                {"id": 615064591, "state": "EXECUTED", "date": "2018-10-14T08:21:33.419441"},
-            ],
-            [
-                {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
-                {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
-                {"id": 615064591, "state": "EXECUTED", "date": "2018-10-14T08:21:33.419441"},
-                {"id": 594226727, "state": "EXECUTED", "date": "2018-09-12T21:27:25.241689"},
-                {"id": 594226727, "state": "EXECUTED", "date": "2018-09-12T21:27:25.241689"},
-                {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
-            ],
+                [
+                    {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+                    {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+                    {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
+                    {"id": 594226727, "state": "EXECUTED", "date": "2018-09-12T21:27:25.241689"},
+                    {"id": 594226727, "state": "EXECUTED", "date": "2018-09-12T21:27:25.241689"},
+                    {"id": 615064591, "state": "EXECUTED", "date": "2018-10-14T08:21:33.419441"},
+                ],
+                [
+                    {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+                    {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+                    {"id": 615064591, "state": "EXECUTED", "date": "2018-10-14T08:21:33.419441"},
+                    {"id": 594226727, "state": "EXECUTED", "date": "2018-09-12T21:27:25.241689"},
+                    {"id": 594226727, "state": "EXECUTED", "date": "2018-09-12T21:27:25.241689"},
+                    {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
+                ],
         ),
         (
-            [
-                {"id": 594226727, "state": "EXECUTED", "date": "2020.11.16 15:43:32.249588"},
-                {"id": 41428829, "state": "EXECUTED", "date": "2020-11-15"},
-                {"id": 594226727, "state": "EXECUTED", "date": "2020/11/14 15:43:32.249588"},
-                {"id": 939719570, "state": "EXECUTED", "date": "2020-10-17 09:40:33.614581+02:00"},
-                {"id": 615064591, "state": "EXECUTED", "date": "Thu Oct 17 17:10:28 2019"},
-                {"id": 615064591, "state": "EXECUTED", "date": "Thursday, 17. October 2019 5:10PM"},
-                {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
-            ],
-            [
-                {"id": 594226727, "state": "EXECUTED", "date": "2020-11-16T15:43:32.249588"},
-                {"id": 41428829, "state": "EXECUTED", "date": "2020-11-15T00:00:00.000000"},
-                {"id": 594226727, "state": "EXECUTED", "date": "2020-11-14T15:43:32.249588"},
-                {"id": 939719570, "state": "EXECUTED", "date": "2020-10-17T09:40:33.614581"},
-                {"id": 615064591, "state": "EXECUTED", "date": "2019-10-17T17:10:28.000000"},
-                {"id": 615064591, "state": "EXECUTED", "date": "2019-10-17T17:10:00.000000"},
-                {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
-            ],
+                [
+                    {"id": 594226727, "state": "EXECUTED", "date": "2020.11.16 15:43:32.249588"},
+                    {"id": 41428829, "state": "EXECUTED", "date": "2020-11-15"},
+                    {"id": 594226727, "state": "EXECUTED", "date": "2020/11/14 15:43:32.249588"},
+                    {"id": 939719570, "state": "EXECUTED", "date": "2020-10-17 09:40:33.614581+02:00"},
+                    {"id": 615064591, "state": "EXECUTED", "date": "Thu Oct 17 17:10:28 2019"},
+                    {"id": 615064591, "state": "EXECUTED", "date": "Thursday, 17. October 2019 5:10PM"},
+                    {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+                ],
+                [
+                    {"id": 594226727, "state": "EXECUTED", "date": "2020-11-16T15:43:32.249588"},
+                    {"id": 41428829, "state": "EXECUTED", "date": "2020-11-15T00:00:00.000000"},
+                    {"id": 594226727, "state": "EXECUTED", "date": "2020-11-14T15:43:32.249588"},
+                    {"id": 939719570, "state": "EXECUTED", "date": "2020-10-17T09:40:33.614581"},
+                    {"id": 615064591, "state": "EXECUTED", "date": "2019-10-17T17:10:28.000000"},
+                    {"id": 615064591, "state": "EXECUTED", "date": "2019-10-17T17:10:00.000000"},
+                    {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+                ],
         ),
     ],
 )
 def test_sort_by_date_positive(list_d, result):
     assert sort_by_date(list_d) == result
+
+
+def test_process_bank_search_title(data_list):
+    assert process_bank_search(data_list, "Перевод") == [
+        {
+            "id": 939719570,
+            "state": "EXECUTED",
+            "date": "2018-06-30T02:08:58.425572",
+            "operationAmount": {
+                "amount": "9824.07",
+                "currency": {
+                    "name": "USD",
+                    "code": "USD"
+                }
+            },
+            "description": "Перевод организации",
+            "from": "Счет 75106830613657916952",
+            "to": "Счет 11776614605963066702"
+        },
+        {
+            "id": 142264268,
+            "state": "EXECUTED",
+            "date": "2019-04-04T23:20:05.206878",
+            "operationAmount": {
+                "amount": "79114.93",
+                "currency": {
+                    "name": "USD",
+                    "code": "USD"
+                }
+            },
+            "description": "Перевод со счета на счет",
+            "from": "Счет 19708645243227258542",
+            "to": "Счет 75651667383060284188"
+        }]
+
+
+def test_process_bank_search_lower(data_list):
+    assert process_bank_search(data_list, "вклад") == [
+        {
+            "id": 587085106,
+            "state": "EXECUTED",
+            "date": "2018-03-23T10:45:06.972075",
+            "operationAmount": {
+                "amount": "48223.05",
+                "currency": {
+                    "name": "руб.",
+                    "code": "RUB"
+                }
+            },
+            "description": "Открытие вклада",
+            "to": "Счет 41421565395219882431"
+        },
+    ]
+
+
+def test_process_bank_search_upper(data_list):
+    assert process_bank_search(data_list, "ВКЛАД") == [
+        {
+            "id": 587085106,
+            "state": "EXECUTED",
+            "date": "2018-03-23T10:45:06.972075",
+            "operationAmount": {
+                "amount": "48223.05",
+                "currency": {
+                    "name": "руб.",
+                    "code": "RUB"
+                }
+            },
+            "description": "Открытие вклада",
+            "to": "Счет 41421565395219882431"
+        },
+    ]
+
+
+def test_process_bank_search_not(data_list):
+    assert process_bank_search(data_list, "обналичивание") == []
+
+
+def test_process_bank_operations(data_list, categories_list):
+    result = process_bank_operations(data_list, categories_list)
+    assert result == {"Перевод со счета на счет": 1, "Открытие вклада": 1, "Перевод организации": 1}
+
+
+def test_process_bank_operations_excess(data_list, categories_list_excess):
+    result = process_bank_operations(data_list, categories_list_excess)
+    assert result == {"Перевод организации": 1, "Обналичивание": 0, "Неизвестная операция": 0}
